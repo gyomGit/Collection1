3e2cc909cae1091efe672468d61575e9287792cd
gyomGit <gyomb@yahoo.fr>
2019-05-01 00:06:44 +0200

    Export Excel file with one image for each item row !

--- Collection1/src/application/MainController.java
+++ Collection1/src/application/MainController.java
@@ -5,9 +5,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 
+import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Drawing;
+import org.apache.poi.ss.usermodel.Picture;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -97,7 +104,7 @@ public class MainController {
 
 	@FXML
 	private TextField localisationField;
-	
+
 	@FXML
 	private TextField searchField;
 
@@ -130,7 +137,7 @@ public class MainController {
 	// --------------------------------------
 
 //	final ObservableList<Contact> data = FXCollections.observableArrayList();
-	
+
 	// -----------------------------------------
 	private ContactController controller = new ContactController();
 
@@ -142,8 +149,8 @@ public class MainController {
 
 	private byte[] imageBytes;
 
-	ObservableList<String> prefixeList = FXCollections.observableArrayList("", "ADN", "CEC", "CG04",
-			"EXPO", "FOR", "MAR", "MDLV", "MGD", "MMHV", "MMV", "MPGV", "MST", "SIST", "SLG", "UBAY");
+	ObservableList<String> prefixeList = FXCollections.observableArrayList("", "ADN", "CEC", "CG04", "EXPO", "FOR",
+			"MAR", "MDLV", "MGD", "MMHV", "MMV", "MPGV", "MST", "SIST", "SLG", "UBAY");
 
 	@FXML
 	private ChoiceBox<String> prefixeBox;
@@ -190,7 +197,7 @@ public class MainController {
 	@SuppressWarnings("resource")
 	@FXML
 	private void handleExport(ActionEvent event) {
-		
+
 		Alert alert1 = new Alert(AlertType.INFORMATION);
 		alert1.setTitle("Information Dialog");
 		alert1.setHeaderText(null);
@@ -200,10 +207,10 @@ public class MainController {
 		DirectoryChooser directoryChooser = new DirectoryChooser();
 		File selectedDirectory = directoryChooser.showDialog(null);
 
-		if(selectedDirectory == null){
-		     //No Directory selected
-		}else{
-			
+		if (selectedDirectory == null) {
+			// No Directory selected
+		} else {
+
 			try {
 //				System.out.print("employees Selected : [");
 //				for (Contact contact : table.getItems()) {
@@ -214,32 +221,100 @@ public class MainController {
 //				System.out.print(" ]\n");
 
 				XSSFWorkbook wb = new XSSFWorkbook(); // for earlier version use HSSF
+
+
+
+				CreationHelper helper = wb.getCreationHelper();
+
+				// create sheet
 				XSSFSheet sheet = wb.createSheet("Objets selection");
+
+
+
+				// auto-size picture relative to its top-left corner
+
 				XSSFRow header = sheet.createRow(0);
 				header.createCell(0).setCellValue("Objet ID");
 				header.createCell(1).setCellValue("Identification");
 				header.createCell(2).setCellValue("Préfixe Muséee");
 				header.createCell(3).setCellValue("Inventaire");
 				header.createCell(4).setCellValue("Localisation");
+				header.createCell(5).setCellValue("Image");
 
 				sheet.autoSizeColumn(1);
 				sheet.autoSizeColumn(2);
 				sheet.autoSizeColumn(3);
 				sheet.autoSizeColumn(4);
+				sheet.autoSizeColumn(5);
+				int widthUnits = 8 * 256;
+				sheet.setColumnWidth(5, widthUnits);
 
 				int index = 1;
 				for (Contact contact : table.getItems())
 					if (contact.getSelected()) {
 						XSSFRow row = sheet.createRow(index);
+						row.setHeightInPoints(40);
+						
 						row.createCell(0).setCellValue(contact.getObjetId().toString());
 						row.createCell(1).setCellValue(contact.getIdentification());
 						row.createCell(2).setCellValue(contact.getPrefixeMusee());
 						row.createCell(3).setCellValue(contact.getInventaire());
 						row.createCell(4).setCellValue(contact.getLocalisation());
+
+
+
+						byte[] getImageInBytes2 = contact.getImage(); // image convert in byte form
+
+						try {
+							FileOutputStream outputstream = new FileOutputStream(new File("photo2.jpg"));
+							outputstream.write(getImageInBytes2);
+
+							Image image = new Image("file:photo2.jpg");
+
+							outputstream.close();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						
+						// add picture data to this workbook.
+						InputStream is = new FileInputStream("photo2.jpg");
+						byte[] bytes = IOUtils.toByteArray(is);
+						int pictureIdx = wb.addPicture(bytes, Workbook.PICTURE_TYPE_JPEG);
+						is.close();
+						
+						// Create the drawing patriarch. This is the top level container for all shapes.
+						@SuppressWarnings("rawtypes")
+						Drawing drawing = sheet.createDrawingPatriarch();
+
+						// add a picture shape
+						ClientAnchor anchor = helper.createClientAnchor();
+						// set top-left corner of the picture,
+						// subsequent call of Picture#resize() will operate relative to it
+//					    anchor.setCol1(3);
+//					    anchor.setRow1(2);
+
+//					    anchor.setCol1(1); //Column B
+//					    anchor.setRow1(2); //Row 3
+//					    anchor.setCol2(2); //Column C
+//					    anchor.setRow2(3); //Row 4
+
+						anchor.setCol1(5); // Column F
+						
+		
+						anchor.setRow1(index); // Row 2	
+		
+						
+//					    anchor.setCol2(6); //Column C
+//					    anchor.setRow2(2); //Row 3
+						Picture pict = drawing.createPicture(anchor, pictureIdx);
+						
+						
+						pict.resize();
 						index++;
 					}
 
-				FileOutputStream fileOut = new FileOutputStream(selectedDirectory.getAbsolutePath() + "/ObjetSelection1.xlsx"); // before 2007 version xls
+				FileOutputStream fileOut = new FileOutputStream(
+						selectedDirectory.getAbsolutePath() + "/ObjetSelection1.xlsx"); // before 2007 version xls
 				wb.write(fileOut);
 				fileOut.close();
 
@@ -255,7 +330,6 @@ public class MainController {
 				e.printStackTrace();
 			}
 		}
-		
 
 	}
 
@@ -263,17 +337,34 @@ public class MainController {
 	@FXML
 	private void handleImport(ActionEvent event) throws IOException {
 
-		try {
+		Alert alert1 = new Alert(AlertType.INFORMATION);
+		alert1.setTitle("Information Dialog");
+		alert1.setHeaderText(null);
+		alert1.setContentText("Veuiller choisir un fichier au format .xlsx");
+		alert1.showAndWait();
 
-			Session s = HibernateUtil.openSession();
+		FileChooser fileChooser = new FileChooser();
 
-			FileInputStream fileIn = new FileInputStream(new File("ObjetInfo.xlsx"));
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Ecxcel files", "*.xlsx"),
+				new ExtensionFilter("All files", "*.*"));
 
-			XSSFWorkbook wb = new XSSFWorkbook(fileIn);
-			XSSFSheet sheet = wb.getSheetAt(0);
-			Row row;
-			for (int i = 1; i < sheet.getLastRowNum(); i++) {
-				row = (Row) sheet.getRow(i);
+		File file = fileChooser.showOpenDialog(null);
+
+		if (file == null) {
+			// No Directory selected
+		} else {
+
+			try {
+
+				Session s = HibernateUtil.openSession();
+
+				FileInputStream fileIn = new FileInputStream(file);
+
+				XSSFWorkbook wb = new XSSFWorkbook(fileIn);
+				XSSFSheet sheet = wb.getSheetAt(0);
+				Row row;
+				for (int i = 1; i < sheet.getLastRowNum(); i++) {
+					row = (Row) sheet.getRow(i);
 
 //				String objetId;
 //				if (row.getCell(0) == null) {
@@ -281,56 +372,57 @@ public class MainController {
 //				} else
 //					objetId = row.getCell(0).toString();
 
-				String identification;
-				if (row.getCell(1) == null) {
-					identification = "null";
-				} // suppose excel cell is empty then its set to 0 the variable
-				else
-					identification = row.getCell(1).toString(); // else copies cell data to name variable
+					String identification;
+					if (row.getCell(1) == null) {
+						identification = "null";
+					} // suppose excel cell is empty then its set to 0 the variable
+					else
+						identification = row.getCell(1).toString(); // else copies cell data to name variable
 
-				String prefixeMusee;
-				if (row.getCell(2) == null) {
-					prefixeMusee = "null";
-				} else
-					prefixeMusee = row.getCell(2).toString();
+					String prefixeMusee;
+					if (row.getCell(2) == null) {
+						prefixeMusee = "null";
+					} else
+						prefixeMusee = row.getCell(2).toString();
 
-				String inventaire;
-				if (row.getCell(3) == null) {
-					inventaire = "null";
-				} else
-					inventaire = row.getCell(3).toString();
+					String inventaire;
+					if (row.getCell(3) == null) {
+						inventaire = "null";
+					} else
+						inventaire = row.getCell(3).toString();
 
-				String localisation;
-				if (row.getCell(4) == null) {
-					localisation = "null";
-				} else
-					localisation = row.getCell(4).toString();
+					String localisation;
+					if (row.getCell(4) == null) {
+						localisation = "null";
+					} else
+						localisation = row.getCell(4).toString();
 
-				s.beginTransaction();
-				Contact c = new Contact();
+					s.beginTransaction();
+					Contact c = new Contact();
 //				c.setObjetId(Integer.parseInt(objetId));
-				c.setIdentification(identification);
-				c.setPrefixeMusee(prefixeMusee);
-				c.setInventaire(inventaire);
-				c.setLocalisation(localisation);
-				System.out.println(c.getObjetId() + " " + c.getIdentification() + " " + c.getPrefixeMusee() + " "
-						+ c.getInventaire() + " " + c.getLocalisation());
-				s.saveOrUpdate(c);
-				s.getTransaction().commit();
+					c.setIdentification(identification);
+					c.setPrefixeMusee(prefixeMusee);
+					c.setInventaire(inventaire);
+					c.setLocalisation(localisation);
+					System.out.println(c.getObjetId() + " " + c.getIdentification() + " " + c.getPrefixeMusee() + " "
+							+ c.getInventaire() + " " + c.getLocalisation());
+					s.saveOrUpdate(c);
+					s.getTransaction().commit();
+				}
+
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Information Dialog");
+				alert.setHeaderText(null);
+				alert.setContentText("Objets Details imported from Excel sheet to Database");
+				alert.showAndWait();
+
+				fileIn.close();
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
 
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle("Information Dialog");
-			alert.setHeaderText(null);
-			alert.setContentText("Objets Details imported from Excel sheet to Database");
-			alert.showAndWait();
-
-			fileIn.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		}
-
 	}
 
 	@FXML
@@ -346,8 +438,8 @@ public class MainController {
 		c.setImage(imageBytes);
 
 		if (validateFields()) {
-		controller.addContact(c);
-		populate();
+			controller.addContact(c);
+			populate();
 		}
 	}
 
@@ -364,24 +456,23 @@ public class MainController {
 		c.setImage(imageBytes);
 
 		if (validateFields()) {
-		controller.updateContact(c);
-		System.out.println("update button clicked");
-		populate();
+			controller.updateContact(c);
+			System.out.println("update button clicked");
+			populate();
 		}
 	}
 
 	@FXML
 	public void handleDelete(ActionEvent event) {
-		
+
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Confirmation Dialog");
 		alert.setHeaderText("Suprimer objet?");
 
-
 		java.util.Optional<ButtonType> result = alert.showAndWait();
-		if (result.get() == ButtonType.OK){
+		if (result.get() == ButtonType.OK) {
 			Contact c = (Contact) controller.getContactList().get(index);
-			controller.removeContact(c.getObjetId()); 
+			controller.removeContact(c.getObjetId());
 			populate();// ... user chose OK
 		} else {
 //			populate(); // ... user chose CANCEL or closed the dialog
@@ -436,9 +527,9 @@ public class MainController {
 	private void populateForm(int i) {
 		if (controller.getContactList().isEmpty())
 			return;
-		
+
 		Contact c = (Contact) controller.getContactList().get(i);
-		
+
 		objetIdField.setText(c.getObjetId().toString());
 		identificationField.setText(c.getIdentification());
 		prefixeBox.setValue(c.getPrefixeMusee());
@@ -481,37 +572,49 @@ public class MainController {
 		localisationCol.setCellValueFactory(new PropertyValueFactory<Contact, String>("localisation"));
 
 		TableColumn<Contact, byte[]> imageCol = new TableColumn<Contact, byte[]>("Image");
-		
+
 		imageCol.setCellValueFactory(new PropertyValueFactory<Contact, byte[]>("image"));
-		
+
+//		imageCol.setCellFactory(new Callback<TableColumn<Contact, Boolean>, TableCell<Contact, Boolean>>() {
+//			public TableCell<Contact, Boolean> call(TableColumn<Contact, Boolean> p) {
 //	       //Set up the ImageView
-//        final ImageView imageview = new ImageView();
-//        imageview.setFitHeight(50);
-//        imageview.setFitWidth(50);
+// 
 //
 //        
 //        
 //        //Set up the Table
-//        TableCell<Contact,Image> cell = new TableCell<Contact,Image>(){
-//                public void updateItem(Contact item, boolean empty) {                        
-//                if(item!=null){
-//                    imageview.setImage(contact.getImage());  //Change suggested earlier
+//        TableCell<Contact,byte[]> cell = new TableCell<Contact,byte[]>(){
+//               
+//        	@Override
+//        	public void updateItem(byte[] item, boolean empty) {                        
+//                if(item == null)
+//                return;
+//                
+//					super.updateItem(item, empty);
+//					if (!isEmpty()) {
+//						final Contact c = getTableView().getItems().get(getIndex());
+//						ImageView imv2 = new ImageView();
+//						
+//						setGraphic(imv2);
+//						
 //                }
 //            }
 //        };
 //
 //        // Attach the imageview to the cell
-//        cell.setGraphic(imageview) 
-//        return cell;
-
+//        cell.setAlignment(Pos.CENTER);
+//        	return cell;
+//		}});
 		TableColumn<Contact, Boolean> selectCol = new TableColumn<Contact, Boolean>("");
 		selectCol.setMinWidth(50);
 		selectCol.setGraphic(getSelectAllCheckBox());
 		selectCol.setCellValueFactory(new PropertyValueFactory<Contact, Boolean>("selected"));
 
 		selectCol.setCellFactory(new Callback<TableColumn<Contact, Boolean>, TableCell<Contact, Boolean>>() {
+			
 			public TableCell<Contact, Boolean> call(TableColumn<Contact, Boolean> p) {
 				final TableCell<Contact, Boolean> cell = new TableCell<Contact, Boolean>() {
+					
 					@Override
 					public void updateItem(final Boolean item, boolean empty) {
 						if (item == null)
@@ -535,7 +638,7 @@ public class MainController {
 				localisationCol, imageCol);
 
 // -------------------------------------------------------------------------------------------------------------		
-		
+
 		ListBinding<Boolean> lb = new ListBinding<Boolean>() {
 			{
 				bind(table.getItems());
@@ -550,7 +653,7 @@ public class MainController {
 				return list;
 			}
 		};
-		
+
 // -------------------------------------------------------------------------------------------------------------		
 		lb.addListener(new ChangeListener<ObservableList<Boolean>>() {
 			@Override
@@ -560,9 +663,8 @@ public class MainController {
 		});
 
 // -------------------------------------------------------------------------------------------------------------			
-		
-	}
 
+	}
 
 	public CheckBox getSelectAllCheckBox() {
 		if (selectAllCheckBox == null) {
@@ -594,96 +696,87 @@ public class MainController {
 		}
 		return this.exportButton;
 	}
-	
 
-	
 	@FXML
-	private void handleSearch() {	
-		
+	private void handleSearch() {
+
 		populate();
-	
+
 		// 1. Wrap the ObservableList in a FilteredList (initially display all data).
 		FilteredList<Contact> filteredData = new FilteredList<>(controller.getContactList(), e -> true);
-		
-		searchField.textProperty().addListener((observableValue, oldValue, newValue) ->{
+
+		searchField.textProperty().addListener((observableValue, oldValue, newValue) -> {
 			filteredData.setPredicate(contact -> {
 				// If filter text is empty, display all contacts.
-				if (newValue == null || newValue.isEmpty()){
+				if (newValue == null || newValue.isEmpty()) {
 					return true;
 				}
 				// Compare first name and last name of every person with filter text.
 				String lowerCaseFilter = newValue.toLowerCase();
-				
-				if(contact.getIdentification().toLowerCase().contains(lowerCaseFilter)){
+
+				if (contact.getIdentification().toLowerCase().contains(lowerCaseFilter)) {
 					return true;// Filter matches identification.
-				}
-				else if(contact.getInventaire().toLowerCase().contains(lowerCaseFilter)) {
+				} else if (contact.getInventaire().toLowerCase().contains(lowerCaseFilter)) {
 					return true;// Filter matches inventaire.
 				}
 				return false;// Does not match.
 			});
 		});
 		// 3. Wrap the FilteredList in a SortedList.
-		SortedList<Contact> sortedData = new SortedList<> (filteredData);
-		
+		SortedList<Contact> sortedData = new SortedList<>(filteredData);
+
 		// 4. Bind the SortedList comparator to the TableView comparator.
 		sortedData.comparatorProperty().bind(table.comparatorProperty());
-		
-	
-		
+
 		table.setItems(sortedData);
-	
-	
-		
-}
-	
+
+	}
+
 	private boolean validateFields() {
-		
-		if(identificationField.getText().isEmpty()) {
+
+		if (identificationField.getText().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Validate fields");
 			alert.setHeaderText(null);
 			alert.setContentText("Le champ Identification est vide");
 			alert.showAndWait();
-		return false;
+			return false;
 		}
-		
-		else if(prefixeBox.getValue().isEmpty()) {
+
+		else if (prefixeBox.getValue().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Validate fields");
 			alert.setHeaderText(null);
 			alert.setContentText("Le Préfixe du Musée n'a pas été sélèctionné");
 			alert.showAndWait();
-		return false;
+			return false;
 		}
-		
-		else if(inventaireField.getText().isEmpty()) {
+
+		else if (inventaireField.getText().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Validate fields");
 			alert.setHeaderText(null);
 			alert.setContentText("Le champ Inventaire est vide");
 			alert.showAndWait();
-		return false;
-		}
-		else if(localisationField.getText().isEmpty()) {
+			return false;
+		} else if (localisationField.getText().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Validate fields");
 			alert.setHeaderText(null);
 			alert.setContentText("Le champ Localisation est vide");
 			alert.showAndWait();
-		return false;
-		}
-		else if(listView.getItems().isEmpty()) {
+			return false;
+		} else if (listView.getItems().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Validate fields");
 			alert.setHeaderText(null);
 			alert.setContentText("La photo n'a pas été sélectionnée pour cet objet");
 			alert.showAndWait();
-		return false;
+			return false;
 		}
-		
+
 		return true;
-		
+
 	}
 
 }
